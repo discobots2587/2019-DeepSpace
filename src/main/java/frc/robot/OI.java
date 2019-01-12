4c1a56c460854ec4244e79683e1677b3e339a2fa
ycyao216 <ycyao216@gmail.com>
2019-01-12 12:41:48 -0600

    updated oi and robot map,and necessary files

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,12 +6,39 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
+import frc.robot.lib.Gamepad;
+import frc.robot.lib.DPADButton;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+//importing all the commands
+
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+
+  public Gamepad p_gp = new Gamepad(0, "Primary");
+  //create all the buttons for primary controller
+	private Button p_btn_RB = new JoystickButton(p_gp, Gamepad.BTN_RB);
+	private Button p_btn_LB = new JoystickButton(p_gp, Gamepad.BTN_LB);
+	private Button p_btn_RT = new JoystickButton(p_gp, Gamepad.BTN_RT);
+	private Button p_btn_LT = new JoystickButton(p_gp, Gamepad.BTN_LT);
+	private Button p_btn_back = new JoystickButton(p_gp, Gamepad.BTN_BACK);
+	private Button p_btn_start = new JoystickButton(p_gp, Gamepad.BTN_START);
+	private Button p_btn_A = new JoystickButton(p_gp, Gamepad.BTN_A);
+	private Button p_btn_X = new JoystickButton(p_gp, Gamepad.BTN_X);
+	private Button p_btn_B = new JoystickButton(p_gp, Gamepad.BTN_B);
+  private Button p_btn_Y = new JoystickButton(p_gp, Gamepad.BTN_Y);
+  /*
+	private Button p_dpad_up = new DPADButton(p_gp, POV.UP);
+	private Button p_dpad_down = new DPADButton(p_gp, POV.DOWN);
+	private Button p_dpad_right = new DPADButton(p_gp, POV.RIGHT);
+  private Button p_dpad_left = new DPADButton(p_gp, POV.LEFT);
+  */
   //// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
   //// joystick.
---
+++ src/main/java/frc/robot/lib/DPADButton.java
@@ -0,0 +1,61 @@
+package frc.robot.lib;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+
+public class DPADButton extends Button {
+
+	private Joystick js;
+	private POV pov;
+	private boolean locked;
+	
+	public enum POV {
+		UP(0), DOWN(180), RIGHT(90), LEFT(270), UP_RIGHT(45), DOWN_RIGHT(135), DOWN_LEFT(225), UP_LEFT(315);
+		
+		private final double val;
+		
+		private POV(double val) {
+			this.val = val;
+		}
+	}
+	
+	/**
+	 * DPADButton from POV direction <i>pov</i> of Joystick <i>js</i>
+	 * <p>
+	 * DPADButton can only be Up, Down, Left, Right
+	 * @param js Joystick
+	 * @param pov pov enum
+	 */
+	public DPADButton(Joystick js, POV pov) {
+		this(js,pov,true);
+	}
+	
+	/**
+	 * DPADButton from POV direction <i>pov</i> of Joystick <i>js</i>
+	 * <p>
+	 * If locked is true, DPADButton can only be Up, Down, Left, Right
+	 * <p>
+	 * If locked is false, DPADButton can only be Up, Down, Left, Right, Up_Right, Up_Left, Down_Right, Down_Left
+	 * @param js Joystick
+	 * @param pov pov enum
+	 * @param locked Locked boolean (only Up, Down, Left, Right)
+	 */
+	public DPADButton(Joystick js, POV pov, boolean locked) {
+		this.js = js;
+		this.pov = pov;
+		
+	}
+	
+	@Override
+	public boolean get() {
+		double val = this.pov.val;
+		double currentVal = js.getPOV();
+		if(this.locked)
+			currentVal = Gamepad.getDPADfromPOV(currentVal);
+		if(currentVal == val) {
+			return true;
+		}
+		return false;
+	}
+
+}
