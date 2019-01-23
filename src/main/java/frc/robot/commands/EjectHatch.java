c5bf3f85a9e1150490536b6e714b163b0945fbb3
ycyao216 <ycyao216@gmail.com>
2019-01-22 23:39:28 -0600

    create intake instance, set execute to push hatch

--- src/main/java/frc/robot/commands/EjectHatch.java
+++ src/main/java/frc/robot/commands/EjectHatch.java
@@ -8,6 +8,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
 public class EjectHatch extends Command {
   public EjectHatch() {
@@ -23,12 +24,13 @@ public class EjectHatch extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.m_hatchIntake.pushHatch();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
 
   // Called once after isFinished returns true
