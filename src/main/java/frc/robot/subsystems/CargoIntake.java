d757e11abe24bd171d780a4aabce5badffadf7c9
DESKTOP-98HPNJR\menon <menonrahul02@gmail.com>
2019-01-30 20:13:34 -0600

    create cargointake constructor with 2 talonsrx's and 2 limit switches

--- src/main/java/frc/robot/subsystems/CargoIntake.java
+++ src/main/java/frc/robot/subsystems/CargoIntake.java
@@ -8,6 +8,14 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 /**
  * Add your docs here.
@@ -15,6 +23,22 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class CargoIntake extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+
+  // initializing two Talon motors to spin intake and raise intake
+  private TalonSRX m_roller;
+  private TalonSRX m_arm;
+
+  // initializing two limit switches to detect when cargo is in place
+  // and when intake is upright
+  private DigitalInput m_rollerSwitch;
+  private DigitalInput m_armSwitch;
+
+  public CargoIntake() {
+    this.m_roller = new TalonSRX(RobotMap.m_rollerMotor);
+    this.m_arm = new TalonSRX(RobotMap.m_armMotor);
+    this.m_rollerSwitch = new DigitalInput(RobotMap.m_cargoRollerLimit);
+    this.m_armSwitch = new DigitalInput(RobotMap.m_cargoArmLimit);
+  }
 
   @Override
   public void initDefaultCommand() {
