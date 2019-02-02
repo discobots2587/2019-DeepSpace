05fad1f0883d84b7e4bad5c1f8104dd7f5451e41
c4seb <casebriscoe@gmail.com>
2019-02-02 13:04:26 -0600

    Moved initilization code from Robot.java to constructor in USBCamera

--- src/main/java/frc/robot/subsystems/USBCamera.java
+++ src/main/java/frc/robot/subsystems/USBCamera.java
@@ -6,6 +6,9 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoSource;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -16,9 +19,25 @@ public class USBCamera extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  private USBCamera camera;
+  private USBCamera camera2;
+
+  public USBCamera(){
+    UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
+    camera.setResolution(160, 120);
+    camera.setFPS(5);
+    camera.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen);
+
+    UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
+    camera2.setResolution(160, 120);
+    camera2.setFPS(5);
+    camera2.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen);
+
+  }
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+    //setDefaultCommand(new MySpecialCommand());
   }
+
 }
