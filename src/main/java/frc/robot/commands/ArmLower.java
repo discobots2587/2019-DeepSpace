b16372984dd802c052e3017bcab16ec14e9fc26a
alarichunz <alaric.hunz@gmail.com>
2019-02-01 19:24:35 -0600

    Implemented the Arm Commands

--- src/main/java/frc/robot/commands/ArmLower.java
+++ src/main/java/frc/robot/commands/ArmLower.java
@@ -8,11 +8,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
-public class Lower extends Command {
-  public Lower() {
+public class ArmLower extends Command {
+  public ArmLower() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+
+    requires(Robot.m_arm);
   }
 
   // Called just before this Command runs the first time
@@ -23,12 +26,13 @@ public class Lower extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.m_arm.lower();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
 
   // Called once after isFinished returns true
