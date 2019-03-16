07bf9a7f8e877a63675d8a477d900f82dca17f8e
c4seb <casebriscoe@gmail.com>
2019-03-16 15:33:23 -0500

    made class for Climb subsystem, added class vars

---
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+/**
+ * Add your docs here.
+ */
+public class Climb extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  DoubleSolenoid m_frontLegs;
+  DoubleSolenoid m_backLegs;
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
