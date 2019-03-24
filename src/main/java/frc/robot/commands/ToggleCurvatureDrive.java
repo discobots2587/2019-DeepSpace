b10ec8baa8765fd4529df4644730d70e38b627f9
c4seb <casebriscoe@gmail.com>
2019-03-24 11:27:05 -0500

    added shell for toggleCurvatureDrive command

--- src/main/java/frc/robot/commands/ToggleCurvatureDrive.java
+++ src/main/java/frc/robot/commands/ToggleCurvatureDrive.java
@@ -9,10 +9,12 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 
+import frc.robot.Robot;
+
 public class ToggleCurvatureDrive extends Command {
   public ToggleCurvatureDrive() {
     // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+    requires(Robot.m_drive);
   }
 
   // Called just before this Command runs the first time
@@ -28,7 +30,7 @@ public class ToggleCurvatureDrive extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
 
   // Called once after isFinished returns true
