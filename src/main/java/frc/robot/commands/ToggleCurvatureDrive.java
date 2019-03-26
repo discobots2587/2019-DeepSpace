85ef65bfc7208175213ada36a6b5abaaad640d30
c4seb <casebriscoe@gmail.com>
2019-03-26 15:23:05 -0500

    Finished toggleCurvatureDrive command

--- src/main/java/frc/robot/commands/ToggleCurvatureDrive.java
+++ src/main/java/frc/robot/commands/ToggleCurvatureDrive.java
@@ -25,6 +25,7 @@ public class ToggleCurvatureDrive extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+   Robot.m_drive.toggleDriveMode(); 
   }
 
   // Make this return true when this Command no longer needs to run execute()
