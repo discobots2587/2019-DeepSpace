81768a81590b48a0292e0a651909dfdcc8752576
casebriscoe <casebriscoe@gmail.com>
2019-03-28 20:59:14 -0500

    added luke's control changes

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -138,10 +138,11 @@ public class OI {
     this.o_btn_back.whenPressed(new ToggleSmartMode());
 
     /* Intake-related Commands */
-    this.o_btn_Y.whileHeld(new IntakeCargo());
-    this.o_btn_Y.whenReleased(new StopRollers());
-    this.o_btn_A.whileHeld(new EjectCargo());
-    this.o_btn_A.whenReleased(new StopRollers());
+    this.o_axisBtn_RT.whenPressed(new IntakeCargo());
+    this.o_axisBtn_RT.whenReleased(new StopRollers());
+    this.o_btn_RB.whenPressed(new EjectCargo());
+    //this.o_btn_A.whileHeld(new EjectCargo());
+    this.o_btn_RB.whenReleased(new StopRollers());
 
     /* Hatch-related Commands */
     this.o_btn_X.whenPressed(new ToggleHatchSideBeak());
@@ -167,7 +168,7 @@ public class OI {
 
     /* Copied to RT/RB triggers as well */
     //this.o_axisBtn_RT.whileHeld(new LiftSetSpeed(-Constants.kMaxLiftSpeed/2)); // Move lift down
-    this.o_axisBtn_RT.whileHeld(new LowerLift(-Constants.kMaxLiftSpeed,
+    /*this.o_axisBtn_RT.whileHeld(new LowerLift(-Constants.kMaxLiftSpeed,
       Constants.kMinLiftPosThreshold,
       Constants.kMinLiftPosThreshold+Constants.kLiftPosThresholdOffset));
     this.o_axisBtn_RT.whenReleased(new LiftSetSpeed(Constants.kLiftHoldSpeed));
@@ -175,7 +176,7 @@ public class OI {
     this.o_btn_RB.whenPressed(new RaiseLift(Constants.kMaxLiftSpeed,
       Constants.kMaxLiftPosThreshold,
       Constants.kMaxLiftPosThreshold-Constants.kLiftPosThresholdOffset));
-    this.o_btn_RB.whenReleased(new LiftSetSpeed(Constants.kLiftHoldSpeed));
+    this.o_btn_RB.whenReleased(new LiftSetSpeed(Constants.kLiftHoldSpeed)); */
   }
 
   public void toggleDriverOperatorControls() {
